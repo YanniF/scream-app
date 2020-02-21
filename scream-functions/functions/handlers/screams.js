7f40d7daff0df5f492fb5a6f9eae499fe17bc8ce
Yanni Fraga <yanni_fernanda@hotmail.com>
2020-02-21 18:05:29 +0100

    Refactoring code

---
+++ scream-functions/functions/handlers/screams.js
@@ -0,0 +1,42 @@
+const { db } = require('../util/admin')
+
+exports.getAllScreams = (req, res) => {
+	db
+		.collection('screams')
+		.orderBy('createdAt', 'desc')
+		.get()
+		.then((data) => {
+			let screams = [];
+
+			data.forEach((doc) => {
+				screams.push({
+					screamId: doc.id,
+					body: doc.data().body,
+					userHandle: doc.data().userHandle,
+					createdAt: doc.data().createAt,
+				});
+			});
+
+			return res.json(screams);
+		})
+		.catch((error) => console.error(error));
+}
+
+exports.postScream = (req, res) => {
+	const newScream = {
+		body: req.body.body,
+		userHandle: req.user.handle,
+		createdAt: new Date().toISOString(),
+	};
+
+	db
+		.collection('screams')
+		.add(newScream)
+		.then((doc) => {
+			res.json({ message: `document ${doc.id} created successfully` });
+		})
+		.catch((error) => {
+			res.status(500).json({ error: 'Something went wrong' });
+			console.error(error);
+		});
+}
--- scream-functions/functions/index.js
+++ scream-functions/functions/index.js
@@ -1,111 +1,18 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+
 const app = require('express')();
-const config = require('./keys/firebaseConfig');
+const fbAuth = require('./util/fbAuth')
 
-admin.initializeApp({
-	credential: admin.credential.cert(require('./keys/admin.json')),
-});
+const { getAllScreams, postScream } = require('./handlers/screams')
+const { signup, login } = require('./handlers/users')
 
-const firebase = require('firebase');
-firebase.initializeApp(config);
+// Scream routes
+app.get('/screams', getAllScreams);
+app.post('/scream', fbAuth, postScream);
 
-const db = admin.firestore();
-
-app.get('/screams', (req, res) => {
-	db
-		.collection('screams')
-		.orderBy('createdAt', 'desc')
-		.get()
-		.then((data) => {
-			let screams = [];
-
-			data.forEach((doc) => {
-				screams.push({
-					screamId: doc.id,
-					body: doc.data().body,
-					userHandle: doc.data().userHandle,
-					createdAt: doc.data().createAt,
-				});
-			});
-
-			return res.json(screams);
-		})
-		.catch((error) => console.error(error));
-});
-
-app.post('/scream', (req, res) => {
-	const newScream = {
-		body: req.body.body,
-		userHandle: req.body.userHandle,
-		createdAt: new Date().toISOString(),
-	};
-
-	db
-		.collection('screams')
-		.add(newScream)
-		.then((doc) => {
-			res.json({ message: `document ${doc.id} created successfully` });
-		})
-		.catch((error) => {
-			res.status(500).json({ error: 'Something went wrong' });
-			console.error(error);
-		});
-});
-
-// sign up route
-let token, userId;
-
-app.post('/signup', (req, res) => {
-	const newUser = {
-		email: req.body.email,
-		password: req.body.password,
-		confirmPassword: req.body.confirmPassword,
-		handle: req.body.handle,
-	};
-
-	// handles must be unique
-	db
-		.doc(`/users/${newUser.handle}`)
-		.get()
-		.then((doc) => {
-			if (doc.exists) {
-				return res.status(400).json({ handle: 'this handle is already taken' });
-			}
-			else {
-				return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
-			}
-		})
-		.then((data) => {
-			userId = data.user.uid;
-			return data.user.getIdToken();
-		})
-		.then((userToken) => {
-			token = userToken;
-
-			const userCredentials = {
-				userId,
-				handle: newUser.handle,
-				email: newUser.email,
-				createdAt: new Date().toISOString(),
-			};
-
-			db.doc(`/users/${newUser.handle}`).set(userCredentials);
-		})
-		.then(() => {
-			return res.status(201).json({ token });
-		})
-		.catch((error) => {
-			console.error(error);
-
-			if (error.code === 'auth/email-already-in-use') {
-				return res.status(400).json({ email: 'Email is already in use' });
-			}
-			else {
-				return res.status(500).json({ error: error.code });
-			}
-		});
-});
+// Users routes
+app.post('/signup', signup)
+app.post('/login', login)
 
 // https://baseurl.com/api/
 exports.api = functions.region('europe-west1').https.onRequest(app);
